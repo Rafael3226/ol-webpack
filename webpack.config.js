ae5ce9539a00fbb187106aca226843ba86848ef6
Rafael <rafael.uribe.2080@gmail.com>
2021-11-30 20:59:31 -0500

    Git Init

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   mode: process.env.NODE_ENV === 'development' ? 'development' : 'production',
   entry: './main.js',
   output: {
-    path: __dirname,
-    filename: 'bundle.js'
+    path: `${__dirname}/build`,
+    filename: 'bundle.js',
   },
 };
